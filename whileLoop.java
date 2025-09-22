b427cb8b6da1cabb5dd840f13652d7f37aacfe00
Nathan <natevictor132286@gmail.com>
2025-09-22 11:07:09 +0300

    While and do while loop

---
+++ whileLoop.java
@@ -0,0 +1,12 @@
+public class whileLoop {
+    public static void main(String[] args) {
+        //while loop executes as long as the condition provided is true
+        int num = 50;
+        int i =1;
+        while(i <= num){
+            System.out.println(i);
+            i++;
+        }
+       // do-while loop executes atleast once then checks the condition
+    }
+}

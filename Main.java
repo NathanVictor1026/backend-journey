0e95c59374f73b21f819d21c1ef395cf78a0ca4a
Nathan <natevictor132286@gmail.com>
2025-09-22 16:19:41 +0300

    Creating arrays using constructors

--- Main.java
+++ Main.java
@@ -1,10 +1,16 @@
 
 public class Main{
         public static void main(String[] args) {
-            Human human1 = new Human("winnie", 21, 60);
+            /*Human human1 = new Human("winnie", 21, 60);
             Human human2 = new Human("Nathan", 25, 85);
 
-            human1.eat();
+            human1.eat();*/
+            Food food1 = new Food("Matooke");
+            Food food2 = new Food("Banana");
+            Food food3 = new Food("Rice");
+
+            Food[] fridge = {food1, food2, food3};
+            System.out.println(fridge[0].name);
         }
 
         }

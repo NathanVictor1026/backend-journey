03789ccbc6e9c55f13a8d8173141947ad2e646f7
Nathan <natevictor132286@gmail.com>
2025-09-22 13:34:52 +0300

    Methods are easy

---
+++ methods.java
@@ -0,0 +1,12 @@
+public class methods {
+    public static void main(String[] args) {
+        // a method is a block of code that is executed when called out within a program
+
+        String name = "Nathan";
+        hello(name);
+    }
+
+    static void hello(String name){
+        System.out.println("Hello "+ name);
+    }
+}

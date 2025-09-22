04ff1b67f5fa0a1f67f57ddee635d7eeb280bd30
Nathan <natevictor132286@gmail.com>
2025-09-22 07:25:30 +0300

    Variables

---
+++ Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+        /* types of variables
+        boolean (-127 to 127)
+        short (-32000 to 32000)
+        int (-2billion to 2billion)
+        long (huge number) always ends with a capital L
+        float (fractional number ends with f)
+        double ( fractional number)
+        char (single letter in single quotes)
+        string (collection of chars in double quotes)
+         */
+        int age = 24; //initialization
+        System.out.println(age);
+    }
+}

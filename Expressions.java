445f464b6fbd4e3066eeb97984d28e38e0c012d5
Nathan <natevictor132286@gmail.com>
2025-09-22 08:01:26 +0300

    Expressions

---
+++ Expressions.java
@@ -0,0 +1,12 @@
+public class Expressions {
+    public static void main(String[] args) {
+        /*expressions = operands and operators
+        operands --> values, variables, quantity
+        operators --> add, divide, modulo etc
+         */
+        // example
+        double friends = 10;
+        friends =  (double) friends / 4;
+        System.out.println(friends);
+    }
+}

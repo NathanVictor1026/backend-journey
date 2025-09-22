2c46e17b5d898f9e799bd31e9c273ab700640219
Nathan <natevictor132286@gmail.com>
2025-09-22 14:56:11 +0300

    Intro to OOP

---
+++ Car.java
@@ -0,0 +1,18 @@
+public class Car {
+    //attributes
+    String make = "Mercedes";
+    String model = "S-class";
+    int year = 2004;
+    String color = "Black";
+    double price = 100000;
+
+    //methods. things that car can perform
+    void drive(){
+        System.out.println("You can drive");
+    }
+
+    void brake(){
+        System.out.println("You can brake");
+    }
+
+}
--- Main.java
+++ Main.java
@@ -1,26 +1,8 @@
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("What is your name? ");
-        String name = input.nextLine();
 
-        System.out.print("How old are you? ");
-        int age = input.nextInt();
+public class Main{
+        public static void main(String[] args) {
+            Car myCar = new Car();
+            System.out.println(myCar.color);
+        }
 
-        input.nextLine();
-
-        System.out.print("What is your favourite food? ");
-        String food = input.nextLine();
-
-        System.out.println("Your name is "+name+ ".");
-        System.out.println("You are "+age+ " years old.");
-        System.out.println("Your favourite food is "+food+ ".");
-        input.close();
-    }
-}
-
-
-
-
-
+        }

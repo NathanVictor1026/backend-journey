034d84a151c6ffd7b33392f9ae51c7e651826c49
Nathan <natevictor132286@gmail.com>
2025-09-22 12:13:07 +0300

    String Methods

---
+++ stringmethods.java
@@ -0,0 +1,14 @@
+public class stringmethods {
+    public static void main(String[] args) {
+        String name = "Nathan";
+        boolean result = name.equals("nathan");
+        if (result == true){
+            System.out.println("Access Granted");
+        }
+        else {
+            System.out.println("denied");
+        }
+        //index of, length, charAt, isEquals, isEmpty, toUpperCase, trim, .replace
+    }
+
+}

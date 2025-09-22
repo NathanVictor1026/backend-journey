073d76a96a5128e61207ad8a4218488fb42c615a
Nathan <natevictor132286@gmail.com>
2025-09-22 11:12:23 +0300

    For Loops

---
+++ forLoop.java
@@ -0,0 +1,10 @@
+public class forLoop {
+    public static void main(String[] args) {
+        //for loop executes a limited amount of times
+        // count from 0 to 10
+        for(int i = 0; i<=10; i++){
+            System.out.println(i);
+        }
+        // unlike a while loop that could run to infinity.
+    }
+}
